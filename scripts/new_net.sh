8f5f60e722ca9192a28675fdc70b5a599653c3cd
Stanisław Maliński <95365847+StanislawMalinski@users.noreply.github.com>
2023-10-07 09:03:10 +0200

    Starting New Project

--- scripts/new_net.sh
+++
@@ -1,8 +0,0 @@
-$switch=$(VBoxManage hostonlyif create | pcregrep -o1 "'(.+)'")
-echo "switch: $switch" >> definitions
-pserver modify --network $Switch --server-ip 10.10.10.1 --netmask 255.0.0.0 --lower-ip 10.10.10.2 --upper-ip 10.10.10.254 --enable
-./new_vm.sh arc mach1 "$switch"
-./new_vm.sh arc mach2 "$switch"
-./new_vm.sh arc mach3 "$switch"
-./new_vm.sh arc mach4 "$switch"
-./new_vm.sh ubu serwer "$switch"
