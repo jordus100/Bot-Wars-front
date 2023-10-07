78a47de45263af9533201e9940f07c6b7b7642f5
Stanisław Maliński <95365847+StanislawMalinski@users.noreply.github.com>
2023-10-07 09:03:38 +0200

    Delete BotWars directory

--- BotWars/src/main/java/Utils/BotRequierments.java
+++
@@ -1,8 +0,0 @@
-package Utils;
-
-public class BotRequierments {
-    public int size; // in kilobytes;
-    public int timeOut; // in seconds
-    public String[] interfaces;
-    public Game type;
-}
