0b1a727b1034e8de6bfdedf2e6fb2866177bf1b0
Mikołaj Burdzy <m.burdzy@odo24.pl>
2023-12-09 16:52:49 +0100

    TournamentEditingForAdminOnly

--- src/Tournaments/TournamentDetails.js
+++ src/Tournaments/TournamentDetails.js
@@ -1,8 +1,10 @@
 import './TournamentDetails.scss'
 import { getListOfTournaments } from './getListOfTournaments';
 import { useParams } from 'react-router-dom';
-
-function TournamentDetails() {
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login, logout } from '../User/actions';
+function TournamentDetails({isAuthenticated, user, login, logout }) {
 
     const { tournamentId } = useParams();
     const tournament = getListOfTournaments().find(t => t.id === parseInt(tournamentId));
@@ -29,7 +31,17 @@ function TournamentDetails() {
                 <div className="tournamentImage"></div>
             </div>
             <p className="tournamentDescription">Opis: {tournament.description}</p>
+            {isAuthenticated ? (
+                <button className="btn"><Link to={`/tournaments/edit/${tournament.id}`}>Edit Tournament</Link></button>
+            ) : null}
         </div>
     );
 }
-export default TournamentDetails;
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.isAuthenticated,
+    user: state.user,
+  });
+  
+const mapDispatchToProps = {login,logout,};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TournamentDetails);
