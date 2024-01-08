6099eaf528e8f05ef040a55ee2e145bd136c9aa7
Jordan <jordan@smartdust.me>
2024-01-08 20:46:27 +0100

    Refactored lists to be reusable

--- src/lists/TournamentsList.js
+++ src/lists/TournamentsList.js
@@ -1,23 +1,19 @@
-import './TournamentsList.scss';
+import './List.scss';
 import DeleteTournamentButton from './DeleteTournamentButton';
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 function TournamentsList({ tournaments, isAuthenticated }) {
     const navigate = useNavigate();
-
-    const handleAddTournamentClick = () => {
-        navigate('/tournaments/add');
-    };
 
     const handleTournamentClick = (tournamentId) => {
         navigate(`/tournaments/details/${tournamentId}`);
     };
     
     const listItems = tournaments.map(tournament => (
-        <div key={tournament.id} className="menu-btns game-list-element btn">
+        <div key={tournament.id} className="menu-btns list-element btn">
             <button 
-                className="game-name color-primary-3 btn"
+                className="item-name color-primary-3 btn"
                 onClick={() => handleTournamentClick(tournament.id)}
             >
                 {tournament.name}
@@ -27,23 +23,16 @@ function TournamentsList({ tournaments, isAuthenticated }) {
     ));
 
     return (
-        <div className="tournaments-container">
+        <div className="list">
             <h1>Tournaments</h1>
             <div className="menu-btns container-list">
-                <div className="list">
-                    {listItems}
-
+                <div className="item-list">
                     {isAuthenticated ? (
-                        <div className="menu-btns game-list-element btn">
-                            {/* Button to add tournament */}
-                            <button 
-                                className="game-name add-game-btn color-primary-3" 
-                                onClick={handleAddTournamentClick}
-                            >
-                                Dodaj turniej
-                            </button>
+                        <div className="menu-btns list-element btn">
+                            <Link className="item-name add-btn color-primary-3" to="/tournaments/add">Dodaj turniej</Link>
                         </div>
-                    ) : null}
+                    ) : (<></>)}
+                    {listItems}
                 </div>
             </div>
         </div>
