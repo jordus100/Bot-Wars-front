697de06ab0cd779f58b80e928b9aeac432b96c8c
Stanleyowskiki <s.t.malinski@gmail.com>
2024-03-17 17:08:04 +0100

    Added more dummy functions

--- src/services/Api.js
+++ src/services/Api.js
@@ -146,6 +146,102 @@ function getUser(request){
         
 }
 
+function getUsersBots(request){
+    //return Request(request, "player", player["functions"]["GetUsersBots"])
+
+    return { "n" : 5,
+        "bots": [{
+            "game": "TicTacToe",
+            "bot": "superBot",
+            "lastModification": "2024-02-06T19:21:17.85",
+            "fileName": "superBot.cpp"
+            },
+            {
+            "game": "Chess",
+            "bot": "chesster",
+            "lastModification": "2024-02-06T19:21:17.85",
+            "fileName": "chesster.cpp"
+            },
+            {
+            "game": "Checkers",
+            "bot": "checky",
+            "lastModification": "2024-02-06T19:21:17.85",
+            "fileName": "checky.cpp"
+            },
+            {
+            "game": "Monopoly",
+            "bot": "monop",
+            "lastModification": "2024-02-06T19:21:17.85",
+            "fileName": "monop.cpp"
+            }
+        ]
+    }
+}
+
+function getAddedGames(request){
+    //return Request(request, "gametype", gametype["functions"]["GetAddedGames"])
+
+    return {
+        "n" : 5,
+        "games": [
+            {
+                "game": "TicTacToe",
+                "lastModification": "2024-02-06T19:21:17.85",
+                "fileName": "TicTacToe.cpp"
+            },
+            {
+                "game": "Chess",
+                "lastModification": "2024-02-06T19:21:17.85",
+                "fileName": "Chess.cpp"
+            },
+            {
+                "game": "Checkers",
+                "lastModification": "2024-02-06T19:21:17.85",
+                "fileName": "Checkers.cpp"
+            },
+            {
+                "game": "Monopoly",
+                "lastModification": "2024-02-06T19:21:17.85",
+                "fileName": "Monopoly.cpp"
+            }
+        ]
+    }
+}
+
+function getUsersPlayedTournaments(request){
+    //return Request(request, "tournament", tournament["functions"]["GetUsersPlayedTournaments"])
+
+    return {
+        "n" : 5,
+        "tournaments": [
+            {
+                "tournament": "TicTacToe",
+                "place": 1,
+                "date": "2024-02-06T19:21:17.85",
+                "reference": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            },
+            {
+                "tournament": "Chess",
+                "place": 2,
+                "date": "2024-02-06T19:21:17.85",
+                "reference": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            },
+            {
+                "tournament": "Checkers",
+                "place": 3,
+                "date": "2024-02-06T19:21:17.85",
+                "reference": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            },
+            {
+                "tournament": "Monopoly",
+                "place": 4,
+                "date": "2024-02-06T19:21:17.85",
+                "reference": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            }
+        ]
+    }
+}
+
 Api.processError = (err) => {
     throw Error(err.message)
 }
@@ -156,5 +252,7 @@ export {
     getAcivmentsForPlayer,
     getPointsOfHistoryForPlayer,
     getUser,
-
+    getUsersBots, 
+    getAddedGames, 
+    getUsersPlayedTournaments,
 }
